538e5859f4f78a7e65b799a0e8b29f5553da6414
James Halliday <mail@substack.net>
2012-02-12 16:59:45 -0800

    .upgrade()

--- index.js
+++ index.js
@@ -29,4 +29,35 @@ Parser.prototype.execute = function (buf, start, len) {
     }
 };
 
+Parser.prototype.upgrade = function () {
+    var self = this;
+    if (self.request
+    && (self.mode === 'begin' || self.mode === 'method'
+    || self.mode === 'url' || self.mode === 'versionBegin'
+    || self.mode === 'version' || self.mode === 'headerField'
+    || self.mode === 'headerValue')) {
+        
+        self.mode = 'upgrade';
+        self.request.emit('upgrade');
+    }
+    else if (self.request) {
+        self.request.once('headers', function () {
+            self.mode = 'upgrade';
+            self.request.emit('upgrade');
+        });
+    }
+    else {
+        var cb = self.cb;
+        self.cb = function () {
+            cb.apply(this, arguments);
+            self.upgrade();
+        };
+    }
+    
+    var ended = false;
+    self.stream.on('end', function () {
+        if (self.request) self.request.emit('end');
+    });
+};
+
 Parser.prototype.modes = require('./lib/modes');
